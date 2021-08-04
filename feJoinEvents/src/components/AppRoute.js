5d3500c0730c7a4b1eab5f398a64a26d18b72866
Billy <billy.freespace@gmail.com>
2021-08-04 14:41:59 -0500

    Agregando EventDetails - en contruccion...

--- feJoinEvents/src/components/AppRoute.js
+++ feJoinEvents/src/components/AppRoute.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Login from '../pages/Login';
-import Register from '../pages/Register';
-import Forget from '../pages/Forget';
-import Principal from '../pages/Principal';
-import ResetPass from '../pages/ResetPass';
-import GenerarEvento from '../pages/GenerarEvento';
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Forget from "../pages/Forget";
+import Principal from "../pages/Principal";
+import ResetPass from "../pages/ResetPass";
+import GenerarEvento from "../pages/GenerarEvento";
+import EventDetails from "../pages/EventDetails";
 
-
-function AppRoute(){
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={Principal} />
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/forget" component={Forget} />
-                <Route exact path="/pasword_reset/:id" component={ResetPass} />
-                <Route exact path="/genevento" component={GenerarEvento} />
-            </Switch>
-        </BrowserRouter>
-    );
+function AppRoute() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Principal} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/forget" component={Forget} />
+        <Route exact path="/pasword_reset/:id" component={ResetPass} />
+        <Route exact path="/genevento" component={GenerarEvento} />
+        <Route exact path="/eventdetails" component={EventDetails} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default AppRoute;
--- feJoinEvents/src/pages/EventDetails.js
+++ feJoinEvents/src/pages/EventDetails.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 
 import Navibar from "../components/navibar.js";
+import NotesIcon from "@material-ui/icons/Notes";
 
 export default class EventDetails extends Component {
   render() {
     return (
-      <div>
-        <Navibar route="Detalles del Evento" />
-      </div>
+      <React.Fragment>
+        <div>
+          <Navibar route="Detalles del Evento" iconRoute={<NotesIcon />} />
+        </div>
+        <div></div>
+      </React.Fragment>
     );
   }
 }
